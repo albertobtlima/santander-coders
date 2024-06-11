fecfe1c7b731379f18898d69981b3f5c45607d4a
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-06-11 16:19:27 -0300

    Create new folder: JS-basico

---
+++ 03-JS-basico/aula01/index.js
@@ -0,0 +1,23 @@
+// Utilize sempre o Camel Case no JavaScript
+// JavaScript possui tipagem dinâmica: infere os tipos de dados
+// JavaScript possui uma tipagem fraca
+// ! Não utilize o var nos seus códigos JavaScript
+
+const nomeDoProfessor = 'Alberto Lima'; // string (texto): Utilize sempre aspas
+const idade = 23; // number
+const altura = 1.77; // number
+const estudando = true; // boolean (booleano: verdadeiro ou falso)
+
+let semConteudo;
+
+console.log(nomeDoProfessor, typeof nomeDoProfessor);
+// console.log(typeof nomeDoProfessor)
+console.log(idade, typeof idade);
+console.log(altura, typeof altura);
+console.log(estudando, typeof estudando);
+console.log(semConteudo);
+
+const curso = 'Front-end em React', topico = 'JavaScript básico';
+console.log(curso);
+console.log(topico);
+console.log(curso, topico);
---
+++ 03-JS-basico/aula01/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const curso = 'Front-end em React', topico = 'JavaScript básico';
+console.log(curso);
+console.log(topico);
+console.log(curso, topico);
