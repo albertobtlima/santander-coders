beefb03abfdc72e894777092d65a8e3751a5d919
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-06-11 16:42:30 -0300

    feat: add aula01

---
+++ 03-JS-basico/aula01/02.js
@@ -0,0 +1,12 @@
+const nota1 = 10;
+const nota2 = 8;
+
+const somaDasNotas = nota1 + nota2;
+let media = (nota1 + nota2) / 2;
+
+console.log('Soma das notas =', somaDasNotas);
+console.log('A média das notas =', media);
+
+const numero = 16;
+const raizDonumero = Math.sqrt(numero);
+console.log('Raiz quadrada de 16 =', raizDonumero);
--- 03-JS-basico/aula01/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const curso = 'Front-end em React', topico = 'JavaScript básico';
-console.log(curso);
-console.log(topico);
-console.log(curso, topico);
