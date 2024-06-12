46b79d59010d2f77ca3d84a49613e43e26e51e40
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-06-12 11:12:42 -0300

    feat: add aula02

---
+++ 03-JS-basico/aula02/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const idadePessoa1 = 15;
+const idadePessoa2 = 39;
+
+console.log(idadePessoa1 > 18);
+console.log(idadePessoa2 > 18);
+console.log(idadePessoa1 >= 18 && idadePessoa2 >= 18);
+console.log(idadePessoa1 >=18 || idadePessoa2 >= 18);
+
+console.log(!true);
+console.log(!(idadePessoa1 > 18));
