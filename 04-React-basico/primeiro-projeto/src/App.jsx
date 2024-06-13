8445210d493daec6458a2fbaad56a88b65817870
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-06-13 18:57:49 -0300

    feat: add images and update articles

--- 04-React-basico/primeiro-projeto/src/App.jsx
+++ 04-React-basico/primeiro-projeto/src/App.jsx
@@ -3,6 +3,10 @@ import "./styles/App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Article } from "./components/Article/Article";
 
+import artcle1Img from "./assets/images/article1.png";
+import artcle2Img from "./assets/images/article2.png";
+import artcle3Img from "./assets/images/article3.png";
+
 // Componente em classe é uma classe que herda a classe Component do React,
 // e retorna HTML dentro do método render.
 
@@ -13,7 +17,30 @@ class App extends React.Component {
       <>
         <Navbar />
         <section id="articles">
-          <Article />
+          <Article 
+            title="Designing Dashboards"
+            provider="NASA"
+            description="Exemplo 01 ipsum dolor sit amet consectetur adipisicing elit. Autem molestias aperiam, temporibus perferendis dolores omnis animi, quasi." 
+            thumbnail={artcle1Img}
+          />
+          <Article 
+            title="Vibrant Portraits" 
+            provider="SpaceNews"
+            description="Exemplo 02 ipsum dolor sit amet consectetur adipisicing elit. Autem molestias aperiam, temporibus perferendis dolores omnis animi, quasi."
+            thumbnail={artcle2Img}
+          />
+          <Article 
+            title="36 Days of Malayalam type" 
+            provider="Spaceflight Now"
+            description="Exemplo 03 ipsum dolor sit amet consectetur adipisicing elit. Autem molestias aperiam, temporibus perferendis dolores omnis animi, quasi."
+            thumbnail={artcle3Img}
+          />
+          <Article
+            title="Exemplo4" 
+            provider="Qualquer"
+            description="Exemplo 04 ipsum dolor sit amet consectetur adipisicing elit. Autem molestias aperiam, temporibus perferendis dolores omnis animi, quasi."
+            thumbnail={artcle1Img}
+          />
         </section>
       </>
     );
--- 04-React-basico/primeiro-projeto/src/components/Article/Article.jsx
+++ 04-React-basico/primeiro-projeto/src/components/Article/Article.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import articleImg from "../../assets/images/article1.png";
 import "./styles.css";
 
 export class Article extends React.Component {
    render() {
       return (
          <article id="article">
-            <img src={articleImg} alt="" />
+            <img src={this.props.thumbnail} alt={this.props.title} />
 
             <div className="article-infos">
-               <h2>Designing Dashboards</h2>
-               <h3>NASA</h3>
-               <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem molestias aperiam, temporibus perferendis dolores omnis animi, enim distinctio sed totam ipsa ab, magnam nam ut suscipit possimus repudiandae. Nemo, quasi.
-               </p>
+               <h2>{this.props.title}</h2>
+               <h3>{this.props.provider}</h3>
+               <p>{this.props.description}</p>
             </div>
          </article>
       )
